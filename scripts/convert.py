d1a1bbf2c21465f21a3f6c69e26ac5ccd8fc74d3
SolmirJafarovich <slava091103@gmail.com>
2025-05-18 01:38:21 +0700

    migrate convertion to onnx-tf backend

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,44 +1,16 @@
 from pathlib import Path
 from typing import Annotated
-from rich.progress import track
 
-import ai_edge_torch
+import onnx
+import tensorflow as tf
 import torch
 import typer
-from torch.ao.quantization.quantize_pt2e import convert_pt2e, prepare_pt2e
-from torch.ao.quantization.quantizer.xnnpack_quantizer import (
-    XNNPACKQuantizer,
-    get_symmetric_quantization_config,
-)
-from torch.utils.data import DataLoader, Subset
-from torchvision import transforms
+from onnx_tf.backend import prepare
 
-from src.config import config, device
-from src.dataset import DepthKeypointDataset
+from src.config import config
 from src.models import BlazePoseLite, CombinedClassifier
-from ai_edge_torch.quantize.quant_config import QuantConfig
 
 app = typer.Typer(pretty_exceptions_enable=False)
-
-
-def calibrate(model):
-    transform = transforms.Compose(
-        [
-            transforms.Resize((config.img_size, config.img_size)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5], [0.5]),
-        ]
-    )
-    dataset = DepthKeypointDataset(transform=transform)
-    data_loader = DataLoader(
-        dataset=Subset(dataset, list(range(500))),
-        batch_size=config.batch,
-        shuffle=True,
-    )
-    with torch.no_grad():
-        for batch in track(data_loader, description="Calibration"):
-            images = batch["image"]
-            _ = model(images.to(torch.float32))
 
 
 @app.command()
@@ -64,48 +36,47 @@ def convert(
     model_fp32.load_state_dict(state_dict)
     model_fp32.eval()
 
-    # === Program capture ===
+    # === .pth -> ONNX ===
 
-    model_fp32 = torch.export.export_for_training(model_fp32, sample_input).module()
-
-    # === Configure quantization ===
-
-    quant_config = get_symmetric_quantization_config()
-    quantizer = XNNPACKQuantizer().set_global(quant_config)
-
-    # === Quantize ===
-
-    model_fp32 = prepare_pt2e(model_fp32, quantizer)
-
-    # TODO calibration
-
-    calibrate(model_fp32)
-
-    model_i8 = convert_pt2e(model_fp32, fold_quantize=True)
-
-    # === Convert to an ai_edge_torch model ===
-
-    tflite_model = ai_edge_torch.convert(
-        module=model_i8,
-        sample_args=sample_input,
-        quant_config=quant_config
+    torch.onnx.export(
+        model=model_fp32,
+        args=sample_input,
+        f=checkpoint / "weights.onnx",  # where should it be saved
+        verbose=False,
+        export_params=True,
+        do_constant_folding=False,  # fold constant values for optimization
+        # do_constant_folding=True,   # fold constant values for optimization
+        input_names=["input"],
+        output_names=["output"],
     )
+    onnx_model = onnx.load(checkpoint / "weights.onnx")
+    onnx.checker.check_model(onnx_model)
 
-    # === Test the model ===
+    # === ONNX -> tensorflow ===
 
-    # Run the prepared model with sample input data to ensure that internal observers are populated with correct values
-    pytorch_output = model_fp32(*sample_input)
-    tflite_output = tflite_model(*sample_input)
+    tf_rep = prepare(onnx_model)  # creating TensorflowRep object
 
-    print("PyTorch output:", pytorch_output)
-    print("TFLite output:", tflite_output)
+    tf_rep.export_graph(checkpoint / "weights.pb")
 
-    # === Save the model ===
+    # === tensorflow -> tflite
 
-    tflite_path = checkpoint / "weights.tflite"
-    tflite_model.export(tflite_path)
+    converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(
+        checkpoint / "weights.pb",
+        input_arrays=["input"],
+        output_arrays=["output"],
+    )
+    converter.experimental_new_converter = True
 
-    typer.echo(f"Готово! Модель сохранена как: {tflite_path}")
+    converter.target_spec.supported_ops = [
+        tf.compat.v1.lite.OpsSet.TFLITE_BUILTINS,
+        tf.compat.v1.lite.OpsSet.SELECT_TF_OPS,
+    ]
+
+    tf_lite_model = converter.convert()
+    with open(checkpoint / "weights.tflite", "wb") as f:
+        f.write(tf_lite_model)
+
+    typer.echo(f"Готово!")
 
 
 if __name__ == "__main__":
